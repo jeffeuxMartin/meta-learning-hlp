2d8de070915363d856a03256ae90e0fcd38f06cc
Jeff Chen <43311603+jeffeuxMartin@users.noreply.github.com>
2021-07-26 06:17:37 +0800

    Wrap all it up.

---
+++ materials/template_md.md
@@ -0,0 +1,23 @@
+# Meta learning for human language processing
+
+||(A) Learning to initialize|(B) Learning to compare|(C) Other|
+|--- |--- |--- |--- |
+|Text Classification|[Dou__COLON__EMNLP19] <br /> [Bansal__COLON__arXiv19] <br /> [holla__DASH__etal__DASH__2020__DASH__learning] <br /> [MetaKD__DASH__arXiv21] <br /> [van__DASH__der__DASH__heijden__DASH__etal__DASH__2021__DASH__multilingual] <br /> [bansal__DASH__etal__DASH__2020__DASH__self] <br /> [murty__DASH__etal__DASH__2021__DASH__dreca]|[Yu__COLON__ACL18] <br /> [Tan__COLON__EMNLP19] <br /> [Geng__COLON__EMNLP19] <br /> [Sun__COLON__EMNLP19] <br /> [geng2020dynamic]|Learning the learning algorithm: <br /> [Wu__COLON__EMNLP19] <br /> Network architecture search: <br /> [pasunuru2020fenas] <br /> [pasunuru2019continual] <br /> Learning to optimize: <br /> [learningToOptimize__COLON__metaNLP21] <br /> Learning to select data: <br /> [label__DASH__correction__DASH__aaai21]|
+|Sequence Labelng|[Wu__COLON__AAAI20] <br /> [xia__DASH__etal__DASH__2021__DASH__metaxl]|[Hou__COLON__ACL20] <br /> [yang2020frustratingly] <br /> [oguz2021few]|Network architecture search: <br /> [li2020learning] <br /> [jiang2019improved]|
+|Relation Classification|[Obamuyide__COLON__ACL19] <br /> [Bose__COLON__arXiv19] <br /> [Lv__COLON__EMNLP19]|[Ye__COLON__ACL19] <br /> [Chen__COLON__EMNLP19] <br /> [Xiong__COLON__EMNLP18] <br /> [Gao__COLON__AAAI19] <br /> [ren2020two]||
+|Knowledge Graph Completion||[xiong2018one] <br /> [wang2019tackling] <br /> [zhang2020few] <br /> [sheng2020adaptive]||
+|Word Embedding|[Hu__COLON__ACL19]|[Sun__COLON__EMNLP18]|Network architecture search: <br /> [li2020learning] <br /> [jiang2019improved]|
+|Question Answering|[mhamdi__DASH__etal__DASH__2021__DASH__x] <br /> [nooralahzadeh__DASH__etal__DASH__2020__DASH__zero] <br /> [yan__DASH__etal__DASH__2020__DASH__multi__DASH__source] <br /> [hua__DASH__etal__DASH__2020__DASH__shot]|||
+|Machine Translation|[Gu__COLON__EMNLP18] <br /> [Indurthi__COLON__arXiv19] <br /> [Li_Wang_Yu_2020] <br /> [unsupervisedMT__DASH__acl21]||Network architecture search: <br /> [wang2020hat] <br /> Learning to select data: <br /> [wang2020balancing] <br /> [pham2020meta]|
+|Parsing|[Guo__COLON__ACL19] <br /> [Huang__COLON__NAACL18] <br /> [multiparsing] <br /> [chen__DASH__etal__DASH__2020__DASH__low] <br /> [wang__DASH__etal__DASH__2021__DASH__meta]|||
+|Dialogue|[Qian__COLON__ACL19] <br /> [Madotto__COLON__ACL19] <br /> [Mi__COLON__IJCAI19] <br /> [Huang__COLON__ACL20] <br /> [dingliwal__DASH__etal__DASH__2021__DASH__shot] <br /> [ST__DASH__dialogue__DASH__AAAI21] <br /> [dai__DASH__etal__DASH__2020__DASH__learning] <br /> [huang__DASH__etal__DASH__2020__DASH__towards__DASH__low]||Learning to optimize: <br /> [Chien__COLON__INTERSPEECH19]|
+|||||
+|Speech Recognition|[Hsu__COLON__ICASSP20] <br /> [Klejch__COLON__ASRU19] <br /> [Winata__COLON__ACL2020] <br /> [Winata__COLON__INTERSPEECH2020] <br /> [ASR__DASH__sample__DASH__AAAI21]|[lux2021meta]|Learning to optimize: <br /> [Klejch__COLON__INTERSPEECH18] <br /> Network architecture search: <br /> [Chen__COLON__INTERSPEECH20] <br /> [Baruwa__COLON__IJSER19]|
+|Source Separation|[SP__DASH__ICASSP21]|||
+|Keyword Spotting|[Chen__COLON__arXiv18]||Network architecture search: <br /> [Mazzawi__COLON__INTERSPEECH19]|
+|Sound Event Detection||[Shimada__COLON__arXiv19] <br /> [Chou__COLON__ICASSP19]||
+|Voice Cloning|||Learning the learning algorithm: <br /> [Chen__COLON__ICLR19] <br /> [Serra__COLON__NeurIPS19]|
+|||||
+|Multi-tasks|||Learning to select data: <br /> [tarunesh2021meta]|
+|Multi-modal||[Eloff__COLON__ICASSP19]|Learning the learning algorithm: <br /> [Suris__COLON__arXiv19] <br /> [learningToLearn__COLON__metaNLP21]|
+
---
+++ src/table_format_parser.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Finished on Mon Jul 26 06:11:06 2021
+
+@filename:    table_format_parser.py
+@author:      Chien-Cheng (Jeff) Chen
+@description: Parse the table Markdown file and bib YAML file into a linked table.
+"""
+
+"""TODO:
+1. Comment and wrapping functions for the script.
+2. `parsed_md = parser__tex_md(ref_texfile)`
+3. `parsed_yaml = parser__bib_yaml(bibtex)`
+4. Auto any bibtex format
+"""
+
+"""DONE:
+1. Deduplicated done
+2. Surname format done (hope to have automatic method...)
+3. Double link done
+4. `{}`s removed
+"""
+
+import yaml
+# NOTE: \[[\w:-]*\]
+# NOTE: '-:_' + 'A-Za-z0-9'
+
+# FIXME: parsed_html = parser__tex_html(ref_texfile)
+# FIXME: parsed_md = parser__html_md(parsed_html)
+parsed_md__FILENAME = "materials/template_md.md"
+with open(parsed_md__FILENAME) as f:
+    parsed_md = f.read()
+
+# FIXME: parsed_yaml = parser__bib_yaml(bibtex)
+parsed_yaml__FILENAME = "materials/bib_source.yaml"
+with open(parsed_yaml__FILENAME) as f:
+    parsed_yaml = yaml.load(f, Loader=yaml.FullLoader)
+
+dup_ref_list = {}
+hungyi_ref_list = {}
+for cite_key in parsed_yaml:
+    cited_item = parsed_yaml[cite_key]
+    conf       = cited_item["conf"     ].strip().replace("\n", ' ').replace("  ", ' ')
+    firstauth  = cited_item["firstauth"].strip().replace("\n", ' ').replace("  ", ' ')
+    fullauth   = cited_item["fullauth" ].strip().replace("\n", ' ').replace("  ", ' ')
+    title      = cited_item["title"    ].strip().replace("\n", ' ').replace("  ", ' ')
+    year       = cited_item["year"     ]
+
+    paper_key = \
+        f"[{firstauth}, {conf}’{str(year)[2:]}]"
+    paper_hungyi_ref = \
+        f"<i>{fullauth}</i>, {title}, {conf}, {year}"  # TODO: any bib?
+    paper_table_form__before = f"[{cite_key}]"
+    paper_list_item = (
+        paper_key, 
+        paper_hungyi_ref,
+        paper_table_form__before,
+        cite_key,
+    )
+    # region Assign key to papers
+    # if key in dup-ref-list:
+    if paper_key in dup_ref_list:
+        # give a new key
+        dup_ref_list[paper_key].append(paper_list_item)
+        real_key = (paper_key[:-1] + 
+            chr(ord('a') + len(dup_ref_list[paper_key]) - 1)) + ']'
+    # elif dup:
+    elif paper_key in hungyi_ref_list:
+        # dup append item
+        existing = hungyi_ref_list.pop(paper_key)
+        dup_ref_list[paper_key] = [
+            existing, paper_list_item]
+        # retrieve a key from dup
+        hungyi_ref_list[
+            paper_key[:-1] + 'a]'] = existing
+        real_key = paper_key[:-1] + 'b]'
+    else:
+        real_key = paper_key
+    # hyref[key] = paper
+    # endregion        
+    hungyi_ref_list[real_key] = paper_list_item
+
+assert len(hungyi_ref_list) == len(parsed_yaml), \
+    "Length different!"
+# print(hungyi_ref_list)
+
+content = parsed_md[:]
+for item in hungyi_ref_list:
+    (
+        paper_key, 
+        paper_hungyi_ref,
+        paper_table_form__before,
+        cite_key,
+    ) = hungyi_ref_list[item]
+    paper_table_form__after = \
+        f"[({item[1:-1]})](#{cite_key})"
+    paper_table_form__wrapped = \
+        f"<span id=\"back__{cite_key}\">{paper_table_form__after}</span>"
+    
+    content = content.replace(
+        paper_table_form__before,
+        paper_table_form__wrapped)
+
+ 
+with open('README.md', 'w') as f:
+    f.write(content)
+    f.write('\n')
+    for item in sorted(hungyi_ref_list):
+        (
+            paper_key, 
+            paper_hungyi_ref,
+            paper_table_form__before,
+            cite_key,
+        ) = hungyi_ref_list[item]
+        paper_list_form__wrapped = \
+            f"- <span id=\"{cite_key}\"> [[{item[1:-1]}]](#back__{cite_key}) {paper_hungyi_ref}</span>"
+        f.write(paper_list_form__wrapped + '\n')
